390614e8fc0e248fa32801088b72f245ddd533a1
xuenobyl <51422805+xuenobyl@users.noreply.github.com>
2023-03-19 17:33:23 +1000

    fix links yet again

--- javascript/script.js
+++ javascript/script.js
@@ -1,14 +1,8 @@
 if (window.location.href.includes("index.html")) {
     $(document).ready(function(){
-        $("#nav").load("html/nav.html");
+        $("#nav").load("html/nav-index.html");
         $("#footer").load("html/footer.html");
     });
-    document.getElementById('index-link').setAttribute("href","index.html");
-    document.getElementById('logo-link').setAttribute("href","images/logo.svg");
-    document.getElementById('about-link').setAttribute('href',"pages/about.html");
-    document.getElementById('classes-link').setAttribute('href',"pages/classes.html");
-    document.getElementById('contact-link').setAttribute('href',"pages/contact.html");
-    document.getElementById('join-link').setAttribute('href',"pages/join.html"); //this is really dumb we should use a different navbar for the index page
 } else {
     $(document).ready(function(){
         $("#nav").load("../html/nav.html");
